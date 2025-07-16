615557a7923ec41a0cac975e19a180111f7d3555
ProgrammingLover71 <abcbcalol69@gmail.com>
2025-07-16 16:50:30 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -14,19 +14,20 @@ class CodeMender:
         def _default(self):
             pass
 
-        def __init__(self, patch_id: str, target: Any, func_name: str):
+        def __init__(self, patch_id: str, target_cls: Any, func_name: str):
             self.original: CodeMender.Function = self._default
-            self.target: Any = target
+            self.target: Any = target_cls
             self.func_name = func_name
             self.id: str = patch_id
+            self._replace_implemented: bool = hasattr(self, "replace")
         
 
-        def prefix(self, target, *orig_args, **orig_kwds):
+        def prefix(self, target, *args, **kwds):
             """This is called before the original function executed."""
-            return orig_args, orig_kwds
+            return args, kwds
 
 
-        def postfix(self, target, call_res: Any, *orig_args, **orig_kwds):
+        def postfix(self, target, call_res: Any, *args, **kwds):
             """This is called after the original function executed."""
             return call_res
         
@@ -43,7 +44,11 @@ class CodeMender:
         def __call__(self, *args, **kwds):
             """Replaces the original class method."""
 
-            new_args, new_kwds = self.prefix(self.target, *args, **kwds)
-            result: Any = self.original(*new_args, **new_kwds)
-            return self.postfix(self.target, result, *args, **kwds)
+            if not self._replace_implemented:            
+                new_args, new_kwds = self.prefix(self.target, *args, **kwds)
+                result: Any = self.original(self.target, *new_args, **new_kwds)
+                return self.postfix(self.target, result, *args, **kwds)
+            else:
+                replace: CodeMender.Function = getattr(self, "replace", self._default)
+                return replace(self.target, *args, *kwds)
 
